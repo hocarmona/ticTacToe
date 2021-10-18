c0a4349c3052bc230b593ac5229c8b2194250d72
Orlando Carmona <orcabe86@gmail.com>
2021-10-18 14:44:23 -0500

    Initial Commit

---
+++ ticTacToe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ticTacToe
+//
+//  Created by Hector Carmona on 10/18/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
